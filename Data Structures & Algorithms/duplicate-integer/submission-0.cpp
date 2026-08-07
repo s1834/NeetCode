7efbf092a1e79a579ba858e801d4e3c4e9251613
Shubh Shah (s1834) <s18324@protonmail.com>
2026-08-07 20:15:42 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        map<int, int> mp;
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+            if(mp[nums[i]] > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
