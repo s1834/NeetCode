adc6e38658075cc83a65225a632816d4abcaf12e
Shubh Shah (s1834) <s18324@protonmail.com>
2026-09-14 00:22:08 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                st.push(s[i]);
+            } else {
+                if (!st.empty()) {
+                    char top = st.top();
+                    if ((s[i] == ')' && top == '(') || (s[i] == ']' && top == '[') || (s[i] == '}' && top == '{')) {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (st.empty()) {
+            return true;
+        }
+        return false;
+    }
+};
