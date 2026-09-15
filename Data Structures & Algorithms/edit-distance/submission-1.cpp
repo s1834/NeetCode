d4725e37b38443629d341fcbaa4e95207dd77b1a
Shubh Shah (s1834) <s18324@protonmail.com>
2026-09-15 22:19:10 +0530

    Add: edit-distance - submission-1

---
+++ Data Structures & Algorithms/edit-distance/submission-1.cpp
@@ -0,0 +1,63 @@
+// Version 1: Recursion + Memoization 1
+// class Solution {
+// private:
+//     int n, m;
+//     vector<vector<int>> dp;
+
+//     int solve(string& word1, string& word2, int i, int j) {
+//         if(i == n) return m - j; // insert in word1
+//         else if(j == m) return n - i; // delete from word2
+        
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         if(word1[i] == word2[j]) return dp[i][j] = solve(word1, word2, i + 1, j + 1);
+//         else {
+//             int ins = 1 + solve(word1, word2, i, j + 1);
+//             int del = 1 + solve(word1, word2, i + 1, j);
+//             int rep = 1 + solve(word1, word2, i + 1, j + 1);
+
+//             return dp[i][j] = min({ins, del, rep});
+//         }
+
+//         return -1;
+//     }
+
+// public:
+//     int minDistance(string word1, string word2) {
+//         n = word1.size();
+//         m = word2.size();
+//         dp.assign(n, vector<int>(m, -1));
+//         return solve(word1, word2, 0, 0);
+//     }
+// };
+
+// Version 2: Recursion + Memoization 2
+class Solution {
+private:
+    vector<vector<int>> dp;
+
+    int solve(string& word1, string& word2, int n, int m) {
+        if(n == 0 || m == 0) return n + m; 
+        
+        if(dp[n][m] != -1) return dp[n][m];
+
+        if(word1[n - 1] == word2[m - 1]) return dp[n][m] = solve(word1, word2, n - 1, m - 1);
+        else {
+            int ins = 1 + solve(word1, word2, n, m - 1);
+            int del = 1 + solve(word1, word2, n - 1, m);
+            int rep = 1 + solve(word1, word2, n - 1, m - 1);
+
+            return dp[n][m] = min({ins, del, rep});
+        }
+
+        return -1;
+    }
+
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        dp.assign(n + 1, vector<int>(m + 1, -1));
+        return solve(word1, word2, n, m);
+    }
+};
